1cdeb79df28e3bf7e1142c968c63b85b8a8483ae
matromen <matromen@naver.com>
2020-05-07 15:41:22 +0900

    webpack.config.js edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
 module.exports = {
-    
+    name: 'wordRelay-webpack',
+    mode: 'development', //production
+    devtool: 'eval',
+
+    entry: {
+    },
+    output: {
+        path: path.join(__dirname, 'dist')
+    }
 }
